94fdec42c737eb5d46766f20d2272e9035c38355
rahoolkar <batman30jan2002@gmail.com>
2025-08-06 13:19:20 +0530

    project phase 1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const port = 3000;
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
@@ -22,6 +23,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.listen(port, () => {
   console.log("app is running on port:", port);
@@ -34,6 +36,12 @@ app.get("/", (req, res) => {
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
 app.get("/listings/new", (req, res) => {
@@ -57,5 +65,30 @@ app.post("/listings", async (req, res) => {
     location,
   });
   await newListing.save();
+  res.redirect("/listings");
+});
+
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, image, price, country, location } = req.body;
+  const updatedListing = await Listing.findByIdAndUpdate(
+    id,
+    {
+      title,
+      description,
+      price,
+      country,
+      image,
+      location,
+    },
+    { new: true, runValidators: true }
+  );
+  res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
   res.redirect("/listings");
 });
